884286a373cbfb9f5ef69a187a9269ad277aa970
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-10-07 16:26:17 -0500

    Initial DO implementation

--- worker.js
+++ worker.js
@@ -1,3 +1,14 @@
 export default {
-  fetch: req => fetch('https://api.cf', req)
+  // fetch: req => new Response(JSON.stringify(req.cf.botManagement))
+  fetch: (req, env) => env.DB.get(env.DB.idFromName(new URL(req.url).hostname)).fetch(req)
+}
+
+export class DB {
+  constructor(state, env) {
+    this.state = state
+    this.env = env
+  }
+  async fetch(req) {
+    return new Response('Hello, world!')
+  }
 }
