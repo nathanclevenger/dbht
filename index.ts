1f5f51e3e72b71cee8fbfffefeb1d28df7b72fe4
Nathan Clevenger <nateclev@gmail.com>
2022-10-07 14:45:15 -0500

    Updated the schema

--- index.ts
+++ index.ts
@@ -3,9 +3,20 @@ export type Schema = {
 }
 
 export type Model = {
+  _id?: keyof Model
   [string]: Primitive | Relationship
 }
 
-export type Primitive = 'bool' | 'string' | 'number' | 'Date'
+export type Primitive = 'bool' | 'string' | 'number' | 'Date' | 'createdBy'
+export type Defaults = 'createdBy' | 'createdAt' | 'updatedBy' | 'updatedAt'
 
-export type Relationship = `{Model}.{keyOf(Model)`
+export type Query<T> = {
+  [string]: string
+}
+
+export type Models = 'Blog' | 'Author' | 'Tag'
+
+
+export const db = (schema: Schema) => {
+  list: query => ({query})
+}
